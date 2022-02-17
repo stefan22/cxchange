b9c9dfd476fcfd31ae38b1b14c549544a8db6778
stefan22 <stefanosemail@protonmail.com>
2022-02-17 23:09:22 +0000

    tests

---
+++ src/components/__tests__/Table.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import CXchange from "../CXchange"
+
+test("renders title", () => {
+    render(<CXchange />);
+    const linkEle = screen.getByText(/Exchange Rates/i);
+    expect(linkEle).toBeInTheDocument();;
+})
