2ef1078af6aea6180ffb248bb3045ab5e7625fa4
stefan22 <stefanosemail@protonmail.com>
2022-02-18 01:51:16 +0000

    tests update

--- src/components/__tests__/AmountField.test.js
+++ src/components/__tests__/AmountField.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import CXchange from "../CXchange"
+import AmountField from "../AmountField"
 
 test("renders title", () => {
     render(<CXchange />);
     const linkEle = screen.getByText(/Exchange Rates/i);
     expect(linkEle).toBeInTheDocument();;
+})
+
+test("displays input label of 'Amount'", () => {
+    render(<AmountField />);
+    const inputLabel = screen.getByLabelText(/Amount:/i);
+    expect(inputLabel).toBeInTheDocument();
 })
--- src/components/__tests__/FiatSelect.test.js
+++ src/components/__tests__/FiatSelect.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import CXchange from "../CXchange"
 
+
 test("renders title", () => {
     render(<CXchange />);
     const linkEle = screen.getByText(/Exchange Rates/i);
     expect(linkEle).toBeInTheDocument();;
 })
+
